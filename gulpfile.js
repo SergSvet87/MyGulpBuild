f952575f6ddc74d39f080942a92683919444d26d
SergSvet87 <94105673+SergSvet87@users.noreply.github.com>
2022-06-19 23:21:10 +0300

    MyGulpBuild

--- gulpfile.js
+++ gulpfile.js
@@ -6,6 +6,8 @@ const cleanCSS = require('gulp-clean-css');
 const babel = require('gulp-babel');
 const uglify = require('gulp-uglify');
 const concat = require('gulp-concat');
+const sourcemaps = require('gulp-sourcemaps');
+const autoprefixer = require('gulp-autoprefixer');
 const del = require('del');
 
 // Пути к файлам проекта
@@ -28,24 +30,33 @@ function clean() {
 // Обработка стилей
 function styles() {
   return gulp.src(paths.styles.src)
+    .pipe(sourcemaps.init())
     .pipe(gulpLess())
-    .pipe(cleanCSS())
+    .pipe(autoprefixer({
+      cascade: false
+    }))
+    .pipe(cleanCSS({
+      level: 2
+    }))
     .pipe(rename({
       basename: 'main',
       suffix: '.min'
     }))
+    .pipe(sourcemaps.write('.'))
     .pipe(gulp.dest(paths.styles.dest))
 }
 
 // Обработка скриптов
 function scripts() {
-  return gulp.src(paths.scriptcs.src, {
-    sourcemaps: true
-  })
-  .pipe(babel())
-  .pipe(uglify())
-  .pipe(concat('main.min.js'))
-  .pipe(gulp.dest(paths.scriptcs.dest))
+  return gulp.src(paths.scriptcs.src)
+    .pipe(sourcemaps.init())
+    .pipe(babel({
+      presets: ['@babel/env']
+    }))
+    .pipe(uglify())
+    .pipe(concat('main.min.js'))
+    .pipe(sourcemaps.write('.'))
+    .pipe(gulp.dest(paths.scriptcs.dest))
 }
 
 // Слежение за изменениями в файлах
